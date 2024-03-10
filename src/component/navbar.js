2869e03e040763b52285f468af89afad795273fd
sameer tamang <94208558+sameerlama5@users.noreply.github.com>
2024-03-10 15:45:00 +0545

    change in style and design

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,31 +1,56 @@
-import { Link } from "react-router-dom";
-import { useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useRef, useEffect, useState } from "react";
+
 function Navbar() {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState("");
+
     const navRef = useRef();
+
+    useEffect(() => {
+        const pathname = location.pathname;
+        setActiveLink(pathname);
+    }, [location.pathname]);
+
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav");
-    }
-    return(
+    };
+
+    return (
         <header>
             <div className="nav-container">
-            <Link className="logoText" to="/">SG.</Link>
-            <nav ref={navRef}>
-                <Link to="/" onClick={showNavbar}>home</Link>
-                <Link to="skillpage" onClick={showNavbar}>Skill</Link>
-                <Link to="projectpage" onClick={showNavbar}>Project</Link>
-                <Link to="/#" onClick={showNavbar}>Blog</Link>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                <i className="fa-solid fa-x"/>
+                <Link className="logoText" to="/">SG.</Link>
+                <nav ref={navRef}>
+
+
+                    <Link to="/" onClick={showNavbar} className={activeLink === "/" ? "active" : ""}>Home</Link>
+
+
+                    <Link to="/skillpage" onClick={showNavbar} className={activeLink === "/skillpage" ? "active" : ""}>Skill</Link>
+
+
+                    <Link to="/projectpage" onClick={showNavbar} className={activeLink === "/projectpage" ? "active" : ""}>Project</Link>
+
+
+                    <Link to="/#" onClick={showNavbar}>Blog</Link>
+
+
+                    <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                        <i className="fa-solid fa-x"/>
+                    </button>
+                </nav>
+
+                <div className="btn-resume">
+                    <Link to="/resumepage">RESUME</Link>
+                </div>
+
+                <button className="nav-btn" onClick={showNavbar}>
+                    <i className="fa-solid fa-bars"/>
                 </button>
-            </nav>
-            <div className="btn-resume">
-            <Link to="resumepage">RESUME</Link>
-            </div>
-            <button className="nav-btn" onClick={showNavbar}>
-            <i className="fa-solid fa-bars"/>
-            </button>
+                
             </div>
         </header>
-    )
+    );
 }
+
 export default Navbar;
